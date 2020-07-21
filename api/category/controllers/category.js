b8b576294c52f0590c37e58e7906200212fb2a33
Selcuk <devonukselcuk@gmail.com>
2020-07-21 19:42:32 +0300

    initial commit

---
+++ api/category/controllers/category.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  //   async find(ctx) {
+  //     const products = await strapi.services.product.find(ctx);
+  //     return products.map((product) => product.title);
+  //   },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '367a312a-6ea3-4798-a025-f96a38df5969'
+};
